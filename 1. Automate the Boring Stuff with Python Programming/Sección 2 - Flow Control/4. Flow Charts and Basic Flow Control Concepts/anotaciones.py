88537e1f79997efeb56764f7dc8b530267bd1064
Collado Claudio <46108725+ccollado7@users.noreply.github.com>
2020-01-10 16:20:53 -0300

    Add files via upload

---
+++ 1. Automate the Boring Stuff with Python Programming/Sección 2 - Flow Control/4. Flow Charts and Basic Flow Control Concepts/anotaciones.py
@@ -0,0 +1,80 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 14:05:16) [MSC v.1915 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> True
+True
+>>> False
+False
+>>> 9
+9
+>>> 99999999
+99999999
+>>> 999999999999
+999999999999
+>>> 9999999999
+9999999999
+>>> "ahahahahsaaaaaaaaaaaaaaaaaa"
+'ahahahahsaaaaaaaaaaaaaaaaaa'
+>>> 42 == 42
+True
+>>> 42 == "Hello"
+False
+>>> 42 == 41
+False
+>>> 2!=3
+True
+>>> 42 < 100
+True
+>>> 42 >= 100
+False
+>>> 42<42
+False
+>>> 42<=42
+True
+>>> myage = 26
+>>> myage <30
+True
+>>> 42 == "42"
+False
+>>> 42.0 == 42
+True
+>>> spam=42
+>>> 42!=42
+False
+>>> 
+>>> 
+>>> 
+>>> True and True
+True
+>>> False and True
+False
+>>> False and False
+False
+>>> 
+>>> 
+>>> True or True
+True
+>>> True or False
+True
+>>> False or False
+False
+>>> 
+>>> 
+>>> not False
+True
+>>> not True
+False
+>>> 
+>>> 
+>>> myage=20
+>>> mypet="cat"myage=20
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> myage=26
+>>> mypet="cat"
+>>> myage >20 and mypet="cat"
+SyntaxError: can't assign to operator
+>>> 
+>>> myage>20 and mypet=="cat"
+True
+>>> 
